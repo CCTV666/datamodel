3bb99be3db341edd279c7ab2a176e14ae9c081b2
一代咩神 <780810441@qq.com>
2020-05-19 05:47:36 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 import setuptools
 
+
+def readme():
+    with open("README.md", encoding="utf-8") as f:
+        return f.read()
+
 setuptools.setup(
     name="datamodel",
-    version="0.0.1",
+    version="0.0.3",
     author="黄江桂",
     author_email="780810441@qq.com",
-    description="datamodel 是专为 Python 写的一个 JSON to Model 模块",
-    long_description="",
+    description="一个可以从 JSON 映射到 Python 类的模块。",
+    long_description=readme(),
     long_description_content_type="text/markdown",
     url="https://github.com/hjzCy/datamodel",
     packages=setuptools.find_packages(),
@@ -16,3 +21,10 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
 )
+
+# 编译：
+# pip install --upgrade setuptools wheel
+# python setup.py sdist bdist_wheel
+# 上传：
+# pip install --upgrade setuptools twine
+# python -m twine upload --repository pypi dist/*
